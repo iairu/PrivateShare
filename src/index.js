db019765c73b556ec649115f0ec8186861422429
Ondrej Spanik <spanik11@msn.com>
2020-10-09 18:48:34 +0200

    Fully working app with CSV load, page load, window re-creation

---
+++ src/index.js
@@ -0,0 +1,106 @@
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
+  app.quit();
+}
+
+let isQuitting = false;
+
+const createBrowseWindow = () => {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    x: 200,
+    y: 0,
+    minimizable: false,
+    maximizable: false,
+    closable: false
+  });
+
+  // Remove app info from User Agent
+  // Google login won't work without this
+  // Broken, userAgent option in loadURL instead
+  // let ua = win.webContents.userAgent;
+  // ua = ua.replace(/privateshare\/[0-9\.-]*\s/,'');
+  // ua = ua.replace(/Electron\/[^\s]*\s/,'');
+  // win.webContents.userAgent = ua;
+
+  win.loadFile(path.join(__dirname, 'browse.html'));
+  win.removeMenu();
+  
+  // Listener for events that currently originate only from menu.js
+  // Has to be on main process because the window contents get entirely changed
+  const bwGoToListener = (e,msg)=>{
+    win.loadURL(msg, {userAgent: 'Mozilla/5.0 (iPod; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/86.0.4240.77 Mobile/15E148 Safari/604.1'});
+  }
+
+  ipcMain.on("bw-goto", bwGoToListener);
+  
+  // YouTube Privacy likes to close the window after submit
+  // The window will just get recreated right afterwards
+  win.on("closed",(e)=>{
+    ipcMain.removeListener("bw-goto", bwGoToListener);
+    if (!isQuitting) createBrowseWindow();
+  })
+  // win.webContents.openDevTools();
+};
+
+const createMenuWindow = () => {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 200,
+    height: 600,
+    x: 0,
+    y: 0,
+    minimizable: false,
+    maximizable: false,
+    webPreferences: {
+        enableRemoteModule: true,
+        nodeIntegration: true
+    }
+  });
+
+  win.loadFile(path.join(__dirname, 'menu.html'));
+  win.removeMenu();
+  win.on("closed",()=>{
+    app.quit();
+  })
+};
+
+const initialize = () => {
+  createBrowseWindow();
+  createMenuWindow();
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', initialize);
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on("before-quit", ()=>{
+  isQuitting = true;
+  app.exit();
+})
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and import them here.
---
+++ src/menu.js
@@ -0,0 +1,54 @@
+const { remote, ipcRenderer } = require("electron");
+const { readFileSync } = require("fs");
+
+
+async function generateNavFromCSV() {
+
+    async function getCSVpath() {
+        return await remote.dialog.showOpenDialog(remote.getCurrentWindow(),{extensions:["csv"]}) // todo: description for CSV, remember last path
+    }
+
+    // async function alert(text) {
+    //     return await remote.dialog.showMessageBox(remote.getCurrentWindow(),{message:text});
+    // }
+
+    function browseWindowGoto(link) {
+        ipcRenderer.send("bw-goto", link);
+    }
+
+    function generatePrivacyEditURL(videoID) {
+        return `https://youtube.com/edit?video_id=${videoID}&nps=1&ar=${Date.now()}&nv=1`;
+    }
+
+    function NavToHTML(linkPairs) {
+        const nav = document.createElement("nav");
+
+        linkPairs.forEach(pair => {
+            broken = (pair[0] == undefined || pair[0] == "") ? true : false;
+            if (!broken) {
+                const link = document.createElement("a");
+                link.href = "#";
+                link.onclick = ()=>{
+                    browseWindowGoto(generatePrivacyEditURL(pair[0]));
+                    link.className = "clicked";
+                }
+                link.innerHTML = pair.length > 0 ? pair[1] : pair[0];
+                nav.appendChild(link);
+            }
+        })
+
+        document.body.appendChild(nav);
+    }
+
+    try {
+        const text = readFileSync((await getCSVpath()).filePaths[0]).toString(); // todo: guarantee utf-8
+        const lines = text.split("\r\n").map(line => line.split(";"));
+        document.body.innerHTML = ""; // Remove possible previous errors
+        NavToHTML(lines);
+    } catch (e) {
+        document.body.innerHTML = "No compatible ids.csv file entered.<br><button onclick=\"generateNavFromCSV()\">Try again</button>"
+    }
+
+}
+
+generateNavFromCSV();
